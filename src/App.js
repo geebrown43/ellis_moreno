35d741d530887875d58df8d575b895dcf11e9672
Gavin Brown <scareb15@gmail.com>
2018-02-12 17:40:02 -0700

    added to Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
-      <div >
-        
+      <div className='app'>
+        <Header/>
       </div>
     );
   }
