b213eb74aadbf5d881413cc69d76a1ce7963df8e
Gavin Brown <scareb15@gmail.com>
2018-02-12 17:40:31 -0700

    adding header

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../App.css'
+import '../index.css'
+
+const Head = () => (
+    <div className='header'>
+        <div className='header_content'>
+            <div id='header_spacing'>
+                <h3 className='subtitle'>Full Stack</h3>
+            </div>
+            <div id='header_spacing'>
+
+                <h1 className='title'>Gavin E. Brown</h1>
+            </div>
+            <div id='header_spacing'>
+
+                <h3 className='subtitle'>Mobile Developer</h3>
+            </div>
+        </div>
+    </div>
+)
+
+export default Head
